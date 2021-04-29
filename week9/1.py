104b166c7992af6c009c2d95263149e4d5522fe3
Ghanashyam Bhat <74894094+Ghanashyam-Bhat@users.noreply.github.com>
2021-04-30 02:17:33 +0530

    Delete week9 directory

--- week9/1.py
+++
@@ -1,4 +0,0 @@
-#arrenging the list of dictionaries in the ascending order of the dictionaries
-l = [{"name":"Virat", "score":10051,"matches":250}, {"name":"rohit", "score":9051,"matches":180}, {"name":"Sachin", "score":18051,"matches":450}]
-l.sort(key="matches")
-print(l)
